11253946da3d3fcbaba343dc51819265bfc4ee25
Mykola Sadlii <mykolac08@gmail.com>
2025-10-03 17:22:38 +0100

    loops

---
+++ js/loops.js
@@ -0,0 +1,53 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+
+// for(let i = 0; i <= 10; i += 1){
+//     if (i === 5) {
+//         continue; // пропуск ітерації
+//     }
+//     console.log(i);
+// }
+
+// const message = "i love frontend";
+// console.log(message[0]);
+// for (let i = 0; i < message.length; i += 1){
+//     console.log(message[i]);
+// }
+
+// for(let i = 0; i <= 10; i += 1){
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// const message = "i love java script";
+// for(let a = 0; a < message.length; a += 1){
+//     if (message[a] === " ") {
+//         continue;
+//     }
+//     console.log(message[a]);
+// }
+
+// Сума чисел від 1 до 100
+// let sum = 0;
+// for(let i = 1; i <= 100; i += 1){
+//     sum += i;
+//     console.log(sum);
+// }
+
+// for(let i = 0; i <= 10; i += 1){
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// 6. Зворотній відлік від 10 до 1
+
+// for(let i = 10; i >= 1; i -= 1){
+//     console.log(i);
+// }
